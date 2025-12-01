5ab2cf98c0e2b9a053dc517a1a78266a9c00027b
mrcallaghan <mcallaghan@naperville203.org>
2025-12-01 08:34:33 -0600

    Started to write notes on ArrayLists

--- ArrayListNotes.java
+++ ArrayListNotes.java
@@ -3,13 +3,18 @@ import java.util.ArrayList;
 /**
  * Class notes on ArrayLists.
  *
- * @author 
- * @version 
+ * @author mrcallaghan
+ * @version 01dec2025
  */
 public class ArrayListNotes
 {
     public static void main(String[] args)
     {
+        ArrayList<Double> myList;
+        myList = createRandomDoubleList(10, 20);
+        System.out.println(myList);
+        removeLessThan(myList, 10.0);
+        System.out.println(myList);
         
     }
     
@@ -25,17 +30,71 @@ public class ArrayListNotes
      */
     public static ArrayList<Double> createRandomDoubleList(int size, int range)
     {
-        return null;
+        /*
+         * ArrayList is a class that represents an ordered collection of elements of the same type.
+         * 
+         * The ArrayList is a Java generic.
+         * 
+         * We have to specify the class type of the elements in the list in angle brackets (i.e., <>)
+         *      after every ArrayList identifier.
+         *      
+         * Primitives (e.g., int, double, boolean) are not classes and cannot be specifed as the type of element
+         *      in a generic. Instead, we can use the corresponding Wrapper Classes (e.g., Integer, Double, Boolean).
+         */
+        ArrayList<Double> list = new ArrayList<Double>();
+        
+        /*
+         * ArrayLists are most similar to lists in Python, and much like lists, ArrayLists have many mutator methods which 
+         *      provide easy access to common list operations, such as add, remove, etc.
+         */
+        
+        for(int i = 0; i < size; i++)
+        {
+            double value = Math.random() * range;
+            /*
+             * The add method adds the specifed object to the end of the list.
+             * 
+             * Autoboxing:
+             *      Primitive values are automatically converted to the thier corresponding wrapper class.
+             *          However, type promotion does not occur.
+             */
+            
+            list.add(value);
+            //list.add(10);  // no type promotion
+        }
+        
+        return list;
     }
     
     /**
      * Removes even numbers from the specified list.
      * 
      * @param list  the list of number of potentially remove
+     * @param limit the number below which to remove numbers
      */
     public static void removeLessThan(ArrayList<Double> list, double limit)
     {
-        
+        /*
+         * The size method returns the number of elements in the list.
+         */
+        for(int i = 0; i < list.size(); i++)
+        {
+            /*
+             * The get method returns the value of the element at the specified index.
+             * 
+             */
+            double value = list.get(i);
+            if(value < limit)
+            {
+                /*
+                 * The remove method deletes the element at the specified index from the list. All subsequent elements
+                 *      are "shifted left".
+                 */
+                list.remove(i);
+                i--;
+                
+            }
+        }
     }
     
     public static void removeLessThanAlt(ArrayList<Double> list, double limit)
